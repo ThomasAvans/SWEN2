7cc7ee2173b67fed9840215ef85dad70318895b6
Lau-Aase <l.aase@student.avans.nl>
2021-12-01 14:59:56 +0100

    Fixed errors main and BubbleSort.h

--- SWEN2_CLI_Application/SWEN2_CLI_Application/BubbleSort.h
+++ SWEN2_CLI_Application/SWEN2_CLI_Application/BubbleSort.h
@@ -2,16 +2,10 @@
 
 class BubbleSort
 {
-<<<<<<< HEAD
     BubbleSort();
     virtual ~BubbleSort();
-    
-};
-=======
 public:
 	void swapping(int& a, int& b);
 	void printSorted(int* array, int size);
 	void sort(int* array, int size);
->>>>>>> origin/Feature_F2
-
 };
