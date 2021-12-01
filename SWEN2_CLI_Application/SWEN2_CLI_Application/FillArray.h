0cd8d778ac9af7caf029cad1d14ad0d5dcedd919
Lau-Aase <l.aase@student.avans.nl>
2021-12-01 14:43:02 +0100

    Created FillArray

--- SWEN2_CLI_Application/SWEN2_CLI_Application/FillArray.h
+++ SWEN2_CLI_Application/SWEN2_CLI_Application/FillArray.h
@@ -1,5 +1,14 @@
-#pragma once
-class FillArray
-{
-};
+#pragma once  
+#include <iostream> 
+#include <cstdlib> 
+#include <ctime>  
 
+class FillArray { 
+    public:      
+    void arrayFiller();     
+    int i;     
+    int array[30];     
+    int odd;     
+    int Max;     
+    int counter = 0; 
+    };
